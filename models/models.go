ab9084a5169cea039d45c6d264d6b23e9a5e410a
Samal <samal.ali@list.ru>
2024-01-29 19:45:01 +0600

    struct

--- models/models.go
+++ models/models.go
@@ -9,4 +9,16 @@ type User struct {
 	Name     string             `json:"name,omitempty"`
 	Email    string             `json:"email,omitempty"`
 	Password string             `json:"password,omitempty"`
+	Portfolio Portfolio          `json:"portfolio,omitempty"`
+}
+
+
+type Portfolio struct {
+	UserID           primitive.ObjectID `json:"userId,omitempty" bson:"userId,omitempty"`
+	DateOfBirth      string             `json:"dateOfBirth,omitempty" bson:"dateOfBirth,omitempty"`
+	Major            string             `json:"major,omitempty"`
+	Bio              string             `json:"bio,omitempty"`
+	AcademicInterests []string           `json:"academicInterests,omitempty"`
+	Skills           []string           `json:"skills,omitempty"`
+	SocialLinks      []string           `json:"socialLinks,omitempty"`
 }
