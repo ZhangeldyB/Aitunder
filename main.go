682c66ea29d30c36fe1583ff02097fabc2ca6d2b
Samal <samal.ali@list.ru>
2024-01-30 13:36:47 +0600

    log

--- main.go
+++ main.go
@@ -6,7 +6,23 @@ import (
 	"io"
 	"net/http"
 	"os"
+
+	"github.com/sirupsen/logrus"
 )
+
+var log = logrus.New()
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&logrus.JSONFormatter{})
+	// Output to a file in addition to stdout.
+	file, err := os.OpenFile("logfile.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err == nil {
+		log.SetOutput(io.MultiWriter(os.Stdout, file))
+	} else {
+		log.Info("Failed to log to file, using default stderr")
+	}
+}
 
 func pageHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
@@ -47,19 +63,7 @@ func testRequest(w http.ResponseWriter, r *http.Request) {
 	message, _ := io.ReadAll(r.Body)
 	w.Write(message)
 }
-func getAllUsersWithPortfolio(w http.ResponseWriter, r *http.Request) {
-    // Call the appropriate function from mongodb package to get all users with portfolio
-    users := mongodb.GetAllUsersWithPortfolioFromDB()
 
-    // Set response headers
-    w.Header().Set("Content-Type", "application/json")
-
-    // Encode users data as JSON and send it in the response
-    if err := json.NewEncoder(w).Encode(users); err != nil {
-        http.Error(w, "Failed to encode users data", http.StatusInternalServerError)
-        return
-    }
-}
 func main() {
 	http.HandleFunc("/main", pageHandler)
 	http.HandleFunc("/login", pageHandler)
@@ -71,7 +75,7 @@ func main() {
 	http.HandleFunc("/api/test", testRequest)
 	http.HandleFunc("/api/profile/add", mongodb.AddUserProfile)
 	http.HandleFunc("/api/getAllUsers", mongodb.GetAllUsers)
-	http.HandleFunc("/api/getAllUsersWithPortfolio", getAllUsersWithPortfolio) 
+
 	fmt.Println("Server is running on http://localhost:8080/main")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
